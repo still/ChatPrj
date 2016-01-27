f001683f09565cdcb17091e7b1447a938566e470
Антипин <antipinav@gmail.com>
2016-01-27 18:15:24 +0300

    Пофиксил возвращение пустой строки в encodeXOR, decodeXOR.

--- encryption.h
+++ encryption.h
@@ -7,7 +7,7 @@
 inline QString encodeXOR(const QString &string, const QString& key)
 {
     if(string.size() == 0 || key.size() == 0)
-        return QString();
+        return string;
     QByteArray encryptedArray;
     QByteArray stringArray(string.toUtf8());
     QByteArray keyArray(key.toUtf8());
@@ -20,7 +20,7 @@ inline QString encodeXOR(const QString &string, const QString& key)
 inline QString decodeXOR(const QString &string, const QString& key)
 {
     if(string.size() == 0 || key.size() == 0)
-        return QString();
+        return string;
     QByteArray encryptedArray;
     QByteArray stringArray = QByteArray::fromBase64(string.toLatin1());
     QByteArray keyArray(key.toUtf8());
